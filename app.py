486a5c393c0d43c1965baacc930c2d764091b899
Prithvi Prakash <93021213+prithvi-prakash@users.noreply.github.com>
2021-12-11 18:46:20 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
+
 from flask import Flask
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World!</p>"
+    return "<p>Hello, World, this is a Heroku Test!</p>"
+
+if __name__ == '__main__':
+    app.run()
